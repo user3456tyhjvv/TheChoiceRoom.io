705e7447d5b8bce6b3eaeb78f43919d5a5d29897
user3456tyhjvv <sammymuigai131@gmail.com>
2025-02-04 12:37:00 +0300

    Updated project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off", // Disable JSX apostrophe warnings
+      "@typescript-eslint/no-unused-vars": "warn", // Change to "off" to fully disable
+    },
+  },
 ];
 
 export default eslintConfig;
